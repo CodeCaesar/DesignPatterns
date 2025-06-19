3cfabfca3c7822c151167df944d9bfe2aa71ba99
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-19 23:34:47 +0100

    Made Felidae abstract class

---
+++ JavaDP/StrategyDesignPattern/Felidae.java
@@ -0,0 +1,15 @@
+package JavaDP.StrategyDesignPattern;
+
+public abstract class Felidae {
+
+    protected String name;
+    /*
+     * Make some Behaviours...
+     * protected behaviour1 behaviour1;
+     * protected behaviour2 behaviour2;
+     * protected behaviour3 behaviour3;
+     */
+
+    public Felidae() {
+    }
+}
