15a7c370af5e34850f6c9ffbad3524cd347c6336
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-20 23:45:41 +0100

    Added behaviours to Felidae class

--- JavaDP/StrategyDesignPattern/Felidae.java
+++ JavaDP/StrategyDesignPattern/Felidae.java
@@ -3,12 +3,8 @@ package JavaDP.StrategyDesignPattern;
 public abstract class Felidae {
 
     protected String name;
-    /*
-     * Make some Behaviours...
-     * protected behaviour1 behaviour1;
-     * protected behaviour2 behaviour2;
-     * protected behaviour3 behaviour3;
-     */
+    protected NoiceBehaviour noiceBehaviour;
+    protected SwimBehaviour swimBehaviour;
 
     public Felidae() {
     }
