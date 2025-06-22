68a50a409551647467683be9e4b3eaceb7857b46
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-22 23:38:29 +0100

    Finished CarFactory.java

--- JavaDP/FactoryDesignPattern/CarFactory.java
+++ JavaDP/FactoryDesignPattern/CarFactory.java
@@ -5,6 +5,13 @@ public class CarFactory {
     public Car createCar(String type) {
         Car car = null;
 
+        if(type.equals("BMW")) {
+            car = new BmwCar();
+        } else if(type.equals("Honda")) {
+            car = new HondaCar();
+        } else if(type.equals("Pagani")) {
+            car = new PaganiCar();
+        }
 
         return car;
     }
