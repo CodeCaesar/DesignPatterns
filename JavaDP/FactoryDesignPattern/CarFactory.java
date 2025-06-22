27dc04dfe7ec48d377b5d3512c03e61daeb438ff
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-23 00:10:31 +0100

    Made CarFactory IF-ELSEIF-ELSE statement alphabetical

--- JavaDP/FactoryDesignPattern/CarFactory.java
+++ JavaDP/FactoryDesignPattern/CarFactory.java
@@ -5,18 +5,18 @@ public class CarFactory {
     public Car createCar(String type) {
         Car car = null;
 
-        if(type.equals("BMW")) {
+        if(type.equals("Audi")) {
+            car = new AudiCar();
+        } else if(type.equals("BMW")) {
             car = new BmwCar();
-        } else if(type.equals("Honda")) {
-            car = new HondaCar();
-        } else if(type.equals("Pagani")) {
-            car = new PaganiCar();
         } else if(type.equals("Ferrari")) {
             car = new FerrariCar();
+        } else if(type.equals("Honda")) {
+            car = new HondaCar();
         } else if(type.equals("Lamborghini") || type.equals("Lambo")) {
             car = new LamborghiniCar();
-        } else if(type.equals("Audi")) {
-            car = new AudiCar();
+        } else if(type.equals("Pagani")) {
+            car = new PaganiCar();
         } else if(type.equals("Toyota")) {
             car = new ToyotaCar();
         }
