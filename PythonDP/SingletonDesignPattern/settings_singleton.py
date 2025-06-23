261635696a848c965f8ec771578939453e322168
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-23 23:34:10 +0100

    Made configure settings for settings_singleton.py

--- PythonDP/SingletonDesignPattern/settings_singleton.py
+++ PythonDP/SingletonDesignPattern/settings_singleton.py
@@ -1,8 +1,12 @@
 class SettingsSingleton:
     _settings_singleton = None
+    settings : dict
 
     def __new__(cls):
         if not cls._settings_singleton:
             cls._settings_singleton = super(SettingsSingleton, cls).__new__(cls)
         
         return cls._settings_singleton
+
+    def configure(self, settings : dict):
+        self.settings = settings
