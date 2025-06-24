62c8aa62527b7c76c66490331fd526068d83be08
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-24 23:17:06 +0100

    Made abstract factory

---
+++ JavaDP/AbstractFactoryDesignPattern/AbstractCarFactory.java
@@ -0,0 +1,10 @@
+package JavaDP.AbstractFactoryDesignPattern;
+
+public class AbstractCarFactory {
+    
+    public CarType createCar(String type) {
+        CarType car = null;
+
+        return car;
+    }
+}
