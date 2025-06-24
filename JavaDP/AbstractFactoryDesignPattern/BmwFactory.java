09012d9586d2ceee89751469739df556fd3361a7
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-24 23:25:36 +0100

    Made BMW factory

---
+++ JavaDP/AbstractFactoryDesignPattern/BmwFactory.java
@@ -0,0 +1,14 @@
+package JavaDP.AbstractFactoryDesignPattern;
+
+public class BmwFactory extends AbstractCarFactory {
+    
+    /**
+     * Might remove @Override
+     */
+    @Override
+    public CarType createCar(String type) {
+        CarType car = null;
+
+        return car;
+    }
+}
